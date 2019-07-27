c8773fb6fc68aa116753ec2b25cc5b3bf201bd43
mohdhaider <mohdhaider1811@gmail.com>
2019-07-27 12:22:26 +0530

    Pods added. Add NYTimes api kesy.

---
+++ NYTimes/Helpers/DataObservers/DataObserver.swift
@@ -0,0 +1,33 @@
+//
+//  DataObserver.swift
+//  NYTimes
+//
+//  Created by Mohd Haider on 27/07/19.
+//  Copyright © 2019 Mohd Haider. All rights reserved.
+//
+
+import Foundation
+
+struct DataObserver<Element> {
+    
+    typealias Listner = (Element) -> ()
+    private var listners = [Listner]()
+    
+    var value: Element {
+        didSet{
+            listners.forEach{ $0(value) }
+        }
+    }
+    
+    init(_ val: Element) {
+        value = val
+    }
+    
+    
+    mutating func bind(_ listner: Listner?) {
+        
+        guard let listnerAvail = listner else { return }
+        
+        listners.append(listnerAvail)
+    }
+}
---
+++ NYTimes/Helpers/Defines/Define.swift
@@ -0,0 +1,13 @@
+//
+//  Define.swift
+//  NYTimes
+//
+//  Created by Mohd Haider on 27/07/19.
+//  Copyright © 2019 Mohd Haider. All rights reserved.
+//
+
+import Foundation
+
+enum NYTimesKeys: String {
+    case apiKey = "Xqr7C1h45kOy745fHfeDFz1i1B2T8px0"
+}
